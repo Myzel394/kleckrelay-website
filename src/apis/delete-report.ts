37b7f39eb5c104e4c7fcb9a3b9068379b363dc64
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2022-11-02 16:05:28 +0100

    added ability to delete reports

---
+++ src/apis/delete-report.ts
@@ -0,0 +1,10 @@
+import {SimpleDetailResponse} from "~/server-types"
+import {client} from "~/constants/axios-client"
+
+export default async function deleteReport(id: string): Promise<SimpleDetailResponse> {
+	const {data} = await client.delete(`${import.meta.env.VITE_SERVER_BASE_URL}/report/${id}`, {
+		withCredentials: true,
+	})
+
+	return data
+}
--- src/route-widgets/ReportDetailRoute/DeleteButton.tsx
+++ src/route-widgets/ReportDetailRoute/DeleteButton.tsx
@@ -1,17 +1,23 @@
 import {ReactElement, useState} from "react"
+import {useNavigate} from "react-router-dom"
+import {useTranslation} from "react-i18next"
+import {MdDelete} from "react-icons/md"
+import {TiCancel} from "react-icons/ti"
+import {AxiosError} from "axios"
+
 import {
-	Alert,
 	Button,
 	Dialog,
 	DialogActions,
 	DialogContent,
 	DialogContentText,
 	DialogTitle,
-	Typography,
 } from "@mui/material"
-import {useTranslation} from "react-i18next"
-import {MdDelete} from "react-icons/md"
-import {TiCancel} from "react-icons/ti"
+import {useMutation} from "@tanstack/react-query"
+
+import {deleteReport} from "~/apis"
+import {useErrorSuccessSnacks} from "~/hooks"
+import {SimpleDetailResponse} from "~/server-types"
 
 export interface DeleteButtonProps {
 	id: string
@@ -19,6 +25,16 @@ export interface DeleteButtonProps {
 
 export default function ReportDetailRoute({id}: DeleteButtonProps): ReactElement {
 	const {t} = useTranslation()
+	const {showError, showSuccess} = useErrorSuccessSnacks()
+	const navigate = useNavigate()
+
+	const {mutate} = useMutation<SimpleDetailResponse, AxiosError, void>(() => deleteReport(id), {
+		onError: showError,
+		onSuccess: () => {
+			showSuccess(t("relations.report.mutations.success.reportDeleted"))
+			navigate("/reports")
+		},
+	})
 
 	const [showDeleteDialog, setShowDeleteDialog] = useState<boolean>(false)
 
@@ -47,7 +63,12 @@ export default function ReportDetailRoute({id}: DeleteButtonProps): ReactElement
 					<Button startIcon={<TiCancel />} onClick={() => setShowDeleteDialog(false)}>
 						{t("general.cancelLabel")}
 					</Button>
-					<Button startIcon={<MdDelete />} color="error">
+					<Button
+						variant="contained"
+						startIcon={<MdDelete />}
+						color="error"
+						onClick={() => mutate()}
+					>
 						{t("routes.ReportDetailRoute.actions.delete.continueAction")}
 					</Button>
 				</DialogActions>
