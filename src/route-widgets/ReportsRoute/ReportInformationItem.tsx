e37eecda9780c1c36cda00f31efb3fa3ba5a9f1a
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2022-11-02 12:50:52 +0100

    improvements

--- src/route-widgets/ReportsRoute/ReportInformationItem.tsx
+++ src/route-widgets/ReportsRoute/ReportInformationItem.tsx
@@ -1,18 +1,16 @@
 import {ReactElement} from "react"
 import {useNavigate} from "react-router-dom"
+import {useTranslation} from "react-i18next"
 
 import {ListItemButton, ListItemText} from "@mui/material"
 
 import {DecryptedReportContent} from "~/server-types"
-import {useTranslation} from "react-i18next"
 
 export interface ReportInformationItemProps {
 	report: DecryptedReportContent
 }
 
-export default function ReportInformationItem({
-	report,
-}: ReportInformationItemProps): ReactElement {
+export default function ReportInformationItem({report}: ReportInformationItemProps): ReactElement {
 	const navigate = useNavigate()
 	const {t} = useTranslation()
 
@@ -20,7 +18,7 @@ export default function ReportInformationItem({
 		<ListItemButton onClick={() => navigate(`/reports/${report.id}`)}>
 			<ListItemText
 				primary={
-					report.messageDetails.content.subject ?? (
+					report.messageDetails.content.subject || (
 						<i>{t("relations.report.emailMeta.emptySubject")}</i>
 					)
 				}
