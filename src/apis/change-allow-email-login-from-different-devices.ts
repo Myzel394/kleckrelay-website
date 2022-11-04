ccdaf12ee3aa7a1d91d539c64db6119368f3024b
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2022-11-04 19:40:29 +0100

    current stand

---
+++ src/apis/change-allow-email-login-from-different-devices.ts
@@ -0,0 +1,27 @@
+import {SimpleDetailResponse} from "~/server-types"
+import {client} from "~/constants/axios-client"
+
+export interface ChangeAllowEmailLoginFromDifferentDevicesData {
+	email: string
+	sameRequestToken: string
+	allow: boolean
+}
+
+export default async function changeAllowEmailLoginFromDifferentDevices({
+	email,
+	sameRequestToken,
+	allow,
+}: ChangeAllowEmailLoginFromDifferentDevicesData): Promise<SimpleDetailResponse> {
+	const {data} = await client.patch(
+		`${
+			import.meta.env.VITE_SERVER_BASE_URL
+		}/auth/login/email-token/allow-email-login-from-different-devices`,
+		{
+			email,
+			sameRequestToken,
+			allow,
+		},
+	)
+
+	return data
+}
