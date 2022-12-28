e94274000429caad0488c5fb72002ffde73654ba
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2022-12-28 18:51:52 +0100

    current stand

---
+++ src/utils/get-encryption-password.ts
@@ -0,0 +1,16 @@
+import * as CryptoJS from "crypto-js"
+
+const SALT_SUFFIX = "accessing_data_you_are_not_allowed_to_is_a_crime"
+
+export default async function getEncryptionPassword(
+	email: string,
+	password: string,
+	salt: string,
+): Promise<CryptoJS.lib.WordArray> {
+	const cryptoSalt = `${salt}-${SALT_SUFFIX}`
+	const cryptoPassword = `${password}-${email}`
+
+	return CryptoJS.PBKDF2(cryptoPassword, cryptoSalt, {
+		keySize: 512 / 32,
+	})
+}
