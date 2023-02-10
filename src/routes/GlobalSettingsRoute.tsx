8f925072ccd1e3aed4bd3378b71720c2dd93d8e5
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-02-10 20:17:12 +0100

    fix: Fix default settings loaded correctly for global settings form

--- src/routes/GlobalSettingsRoute.tsx
+++ src/routes/GlobalSettingsRoute.tsx
@@ -1,15 +1,23 @@
 import {ReactElement} from "react"
-import {useQuery} from "@tanstack/react-query"
-import {AdminSettings} from "~/server-types"
 import {AxiosError} from "axios"
+import _ from "lodash"
+
+import {useQuery} from "@tanstack/react-query"
+
+import {AdminSettings} from "~/server-types"
 import {getAdminSettings} from "~/apis"
 import {QueryResult} from "~/components"
-import {useTranslation} from "react-i18next"
-import SettingsForm from "~/route-widgets/GlobalSettingsRoute/SettingForm"
+import {DEFAULT_ADMIN_SETTINGS} from "~/constants/admin-settings"
+import SettingsForm from "~/route-widgets/GlobalSettingsRoute/SettingsForm"
 
 export default function GlobalSettingsRoute(): ReactElement {
-	const {t} = useTranslation()
-	const query = useQuery<AdminSettings, AxiosError>(["get_admin_settings"], getAdminSettings)
+	const query = useQuery<AdminSettings, AxiosError>(["get_admin_settings"], async () => {
+		const settings = getAdminSettings()
+
+		return _.mergeWith({}, DEFAULT_ADMIN_SETTINGS, settings, (o, s) =>
+			_.isNull(s) ? o : s,
+		) as AdminSettings
+	})
 
 	return (
 		<QueryResult<AdminSettings> query={query}>
