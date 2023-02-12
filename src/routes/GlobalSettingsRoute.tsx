785a712eb78fa2ca075964d05350a8a7f3560a39
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-02-12 21:20:04 +0100

    fix: Loading settings

--- src/routes/GlobalSettingsRoute.tsx
+++ src/routes/GlobalSettingsRoute.tsx
@@ -13,7 +13,7 @@ import SettingsForm from "~/route-widgets/GlobalSettingsRoute/SettingsForm"
 export default function GlobalSettingsRoute(): ReactElement {
 	const queryKey = ["get_admin_settings"]
 	const query = useQuery<AdminSettings, AxiosError>(queryKey, async () => {
-		const settings = getAdminSettings()
+		const settings = await getAdminSettings()
 
 		return _.mergeWith({}, DEFAULT_ADMIN_SETTINGS, settings, (o, s) =>
 			_.isNull(s) ? o : s,
