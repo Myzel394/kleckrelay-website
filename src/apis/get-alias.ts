279406b9c7da94ff343411b05b4c26b0de4f9316
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-02-12 20:25:19 +0100

    fix: Adapt to new API

--- src/apis/get-alias.ts
+++ src/apis/get-alias.ts
@@ -1,8 +1,8 @@
 import {client} from "~/constants/axios-client"
 import {Alias} from "~/server-types"
 
-export default async function getAlias(address: string): Promise<Alias> {
-	const {data} = await client.get(`${import.meta.env.VITE_SERVER_BASE_URL}/v1/alias/${address}`, {
+export default async function getAlias(aliasID: string): Promise<Alias> {
+	const {data} = await client.get(`${import.meta.env.VITE_SERVER_BASE_URL}/v1/alias/${aliasID}`, {
 		withCredentials: true,
 	})
 
