17673852dd468e516e3bc8c7ed1b22edefece20a
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-02-15 22:27:20 +0100

    refactor!: Adapt api fetchers to upstream server

--- src/apis/get-me.ts
+++ src/apis/get-me.ts
@@ -1,7 +1,7 @@
-import {AuthenticationDetails} from "~/server-types"
+import {ServerUser} from "~/server-types"
 import {client} from "~/constants/axios-client"
 
-export default async function getMe(): Promise<AuthenticationDetails> {
+export default async function getMe(): Promise<ServerUser> {
 	const {data} = await client.get(`${import.meta.env.VITE_SERVER_BASE_URL}/v1/account/me`, {
 		withCredentials: true,
 	})
