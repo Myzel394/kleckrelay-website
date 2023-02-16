8a2de63b6a63b9bdc00ba663971a72b476fad003
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-02-16 21:03:20 +0100

    fix: Remove wrong api adaption

--- src/apis/get-server-settings.ts
+++ src/apis/get-server-settings.ts
@@ -1,13 +1,6 @@
 import {ServerSettings} from "~/server-types"
 import {client} from "~/constants/axios-client"
 
-export type GetServerSettingsResponse =
-	| ServerSettings
-	| {
-			detail: string
-			code: "error:settings:statistics_disabled"
-	  }
-
-export default async function getServerSettings(): Promise<GetServerSettingsResponse> {
+export default async function getServerSettings(): Promise<ServerSettings> {
 	return (await client.get(`${import.meta.env.VITE_SERVER_BASE_URL}/v1/server/settings`)).data
 }
