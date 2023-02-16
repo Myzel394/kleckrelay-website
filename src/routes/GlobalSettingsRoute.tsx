9c85cc13ac60751d191642e463f226e481c2828e
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-02-16 22:21:38 +0100

    feat: Show message when global settings are disabled

--- src/routes/GlobalSettingsRoute.tsx
+++ src/routes/GlobalSettingsRoute.tsx
@@ -8,21 +8,32 @@ import {AdminSettings} from "~/server-types"
 import {getAdminSettings} from "~/apis"
 import {QueryResult} from "~/components"
 import {DEFAULT_ADMIN_SETTINGS} from "~/constants/admin-settings"
+import SettingsDisabled from "~/route-widgets/GlobalSettingsRoute/SettingsDisabled"
 import SettingsForm from "~/route-widgets/GlobalSettingsRoute/SettingsForm"
 
 export default function GlobalSettingsRoute(): ReactElement {
 	const queryKey = ["get_admin_settings"]
-	const query = useQuery<AdminSettings, AxiosError>(queryKey, async () => {
-		const settings = await getAdminSettings()
+	const query = useQuery<AdminSettings | null, AxiosError>(queryKey, async () => {
+		const {code, detail, ...settings} = await getAdminSettings()
 
-		return _.mergeWith({}, DEFAULT_ADMIN_SETTINGS, settings, (o, s) =>
-			_.isNull(s) ? o : s,
-		) as AdminSettings
+		if (code === "error:settings:global_settings_disabled") {
+			return null
+		} else {
+			return _.mergeWith({}, DEFAULT_ADMIN_SETTINGS, settings, (o, s) =>
+				_.isNull(s) ? o : s,
+			) as AdminSettings
+		}
 	})
 
 	return (
-		<QueryResult<AdminSettings> query={query}>
-			{settings => <SettingsForm settings={settings} queryKey={queryKey} />}
+		<QueryResult<AdminSettings | null> query={query}>
+			{settings =>
+				settings === null ? (
+					<SettingsDisabled />
+				) : (
+					<SettingsForm settings={settings} queryKey={queryKey} />
+				)
+			}
 		</QueryResult>
 	)
 }
