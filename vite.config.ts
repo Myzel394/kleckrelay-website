0879066e7e2aab73e105b6f750013612116274c9
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-02-19 08:39:15 +0100

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import * as path from "path"
 import {defineConfig} from "vite"
+import {viteCommonjs} from "@originjs/vite-plugin-commonjs"
 
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [viteCommonjs(), react()],
 	resolve: {
 		alias: {
 			"~": path.resolve(__dirname, "src"),
