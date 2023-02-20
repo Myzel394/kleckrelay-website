d8c86584014d18c237d1d5e22a0d81bb18050f4d
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-02-20 20:05:53 +0100

    fix: Cache server settings in sessionStorage

--- src/apis/get-server-settings.ts
+++ src/apis/get-server-settings.ts
@@ -2,5 +2,15 @@ import {ServerSettings} from "~/server-types"
 import {client} from "~/constants/axios-client"
 
 export default async function getServerSettings(): Promise<ServerSettings> {
-	return (await client.get(`${import.meta.env.VITE_SERVER_BASE_URL}/v1/server/settings`)).data
+	const savedData = sessionStorage.getItem("server-settings")
+
+	if (savedData) {
+		return JSON.parse(savedData)
+	}
+
+	const {data} = await client.get(`${import.meta.env.VITE_SERVER_BASE_URL}/v1/server/settings`)
+
+	sessionStorage.setItem("server-settings", JSON.stringify(data))
+
+	return data
 }
