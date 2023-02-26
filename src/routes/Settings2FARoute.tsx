7ec6b81b5e0a111bf9026494837866ab5855ebe3
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-02-26 11:53:08 +0100

    feat: Add disable 2FA

--- src/routes/Settings2FARoute.tsx
+++ src/routes/Settings2FARoute.tsx
@@ -3,9 +3,10 @@ import {AxiosError} from "axios"
 import {useTranslation} from "react-i18next"
 
 import {useQuery} from "@tanstack/react-query"
-import {Alert} from "@mui/material"
+import {Alert, Grid} from "@mui/material"
 
 import {QueryResult, SimplePageBuilder} from "~/components"
+import Delete2FA from "~/route-widgets/Settings2FARoute/Delete2FA"
 import Setup2FA from "~/route-widgets/Settings2FARoute/Setup2FA"
 import getHas2FAEnabled from "~/apis/get-has-2fa-enabled"
 
@@ -19,11 +20,16 @@ export default function Settings2FARoute(): ReactElement {
 			<QueryResult<boolean, AxiosError> query={query}>
 				{has2FAEnabled =>
 					has2FAEnabled ? (
-						<>
-							<Alert severity="success">
-								{t("routes.SettingsRoute.2fa.alreadyEnabled")}
-							</Alert>
-						</>
+						<Grid container spacing={4} direction="column" alignItems="center">
+							<Grid item>
+								<Alert severity="success">
+									{t("routes.SettingsRoute.2fa.alreadyEnabled")}
+								</Alert>
+							</Grid>
+							<Grid item>
+								<Delete2FA onSuccess={query.refetch} />
+							</Grid>
+						</Grid>
 					) : (
 						<Setup2FA onSuccess={query.refetch} />
 					)
